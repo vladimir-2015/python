5f8f6984eb79c2ada7d64630495b503400ac47ec
vladimir-architect <vladimir@architect.kiev.ua>
2015-11-27 13:10:31 +0200

    Программа для расчета факториала созданная с помощью блокли

---
+++ python/factorial.py
@@ -0,0 +1,21 @@
+N = None
+i = None
+Result = None
+
+def text_prompt(msg):
+  try:
+    return raw_input(msg)
+  except NameError:
+    return input(msg)
+
+
+N = float(text_prompt('input N'))
+if N < 0:
+  print('N can not be less zerro')
+else:
+  i = 0
+  Result = 1
+  for count in range(int(N)):
+    i = i + 1
+    Result = Result * i
+  print(Result)
